b6ffc789bce805d11879a18c53a0047656961a4a
Shedriq <jerrydwain4@gmail.com>
2022-12-15 11:17:41 +0300

    task 122

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - builds an avl tree from an array
+ * array_to_avl - builds an AVL tree from an array
  * @array: array to create from
  * @size: size of the array
  *
- * Return: a pointer to the root node of the created avl tree
- * NULL on failure
+ * Return: a pointer to the root node of the created AVL tree
+ *         NULL on failure
  */
-
 avl_t *array_to_avl(int *array, size_t size)
 {
 	unsigned int i;
