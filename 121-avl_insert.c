2711dcae77a1dbeb9c5122a462987ec5f1af8925
Shedriq <jerrydwain4@gmail.com>
2022-12-15 10:15:35 +0300

    task 121

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * r_insert_node - node value insertion in a AVL
- * @tree: type **pointer of root node of the Avl tree struct
- * @parent: parent node of struct avl
- * @new: type **pointer left or right insertion
- * @nval: insertion value of the avl
+ * r_insert_node - node value instertion in a AVL.
+ * @tree: type **pointer of root node of the AVL tree struct.
+ * @parent: parent node of struct AVL.
+ * @new: type**pointer left or right insertion.
+ * @nval: insertion value of the AVL.
  * Return: pointer to the new root after insertion otherwise NULL
  */
-
 avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
 {
 	int bval;
@@ -52,14 +51,12 @@ avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
 	}
 	return (*tree);
 }
-
 /**
- * avl_insert - inserts a value into an avl tree
- * @tree: type **pointer to the root node of the avl tree to insert into
+ * avl_insert - inserts a value into an AVL tree.
+ * @tree: type **pointer to the root node of the AVL tree to insert into.
  * @value: value to store in the node to be inserted
- * Return: inserted node, or NULL if it fails to insert
+ * Return: inserted node, or NULL if fails.
  */
-
 avl_t *avl_insert(avl_t **tree, int value)
 {
 	avl_t *new = NULL;
